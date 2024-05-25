97f36ae40ec04ce1fa2df6edff6308d2199df125
lepusarcticus123 <1035047456@qq.com>
2024-05-25 20:35:35 +0800

    添加注释

--- README.md
+++ README.md
@@ -1,7 +1,6 @@
 # ToDoList
 a **theme-changeable** todo list
-use localstorage to stash your data,and it's version 1.0.0
-
-![image-20240525184616874](C:\Users\10350\AppData\Roaming\Typora\typora-user-images\image-20240525184616874.png)
-
-and this is how it looks(in first theme)
+可切换主题todo list
+原地址：https://www.youtube.com/watch?v=qhjxAP1hFuI
+模仿*youtuber*建造的*todo-list*,还原的同时添加了切换主题的功能，并对代码进行了详细的解读与扩展，非常适合新手（VUE2入门，对VUE3尚不了解的人），在项目中做了详细的思维导图分析（学习历程），并扩展了相应内容，可以无负担学习。
+本人也是新手，功能尚未拓展，只是为了交流学习。
--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,24 @@
 <script setup>
-import { ref, onMounted, computed, watch,nextTick} from 'vue'
+import { ref, onMounted, computed, watch} from 'vue'
 
-const todos = ref([])
-let theme=ref('normal')
-const name = ref('')
-const input_content = ref('')
-const input_category = ref(null)
+//定义变量
+let theme=ref('normal') //定义响应式主题变量
+const todos = ref([])   //存储输入的todo的中转变量
+const name = ref('')    //定义用户名字的变量
+const input_content = ref('') //输入的todo内容变量
+const input_category = ref(null) //输入的类别变量
+
+//计算属性，对所有todo进行排序
 const todos_asc = computed(() => todos.value.sort((a, b) => {
   return a.createdAt - b.createdAt
 }))
+//添加tooo函数
 const addTodo = () => {
+  //判断
   if (input_content.value.trim() === '' || input_category.value === null) {
     return
   }
+  //添加
   todos.value.push({
     content: input_content.value,
     category: input_category.value,
@@ -20,29 +26,40 @@ const addTodo = () => {
     editable: false,
     createdAt: new Date().getTime()
   })
+  //清空输入框
   input_category.value=null
   input_content.value=''
 }
+//删除todo函数
 const removeTodo = (todo) => {
   todos.value = todos.value.filter((t)=>t !==todo)
+  //创建新数组，包含所有通过测试的元素
+  //强调了开发的数据不可变性
+  //创建新数组虽然增加了一些处理压力，但影响非常小
 }
-
+//监视所有todos的改变，将所有todos的变化存储在本地仓库中
 watch(todos, newVal => {
   localStorage.setItem('todos', JSON.stringify(newVal))
 }, {
   deep: true
 })
+//监视所有name的变化，将name存储到本地仓库
 watch(name, (newVal) => {
   localStorage.setItem('name', newVal);
 })
+//监视所有theme的变化，将theme存储到本地仓库
+watch(theme, (newVal) => {
+  localStorage.setItem('theme', newVal)
+})
+//生命周期钩子函数，从本地仓库取出我们所需要的值
 onMounted(() => {
   name.value = localStorage.getItem('name') || ''
   todos.value = JSON.parse(localStorage.getItem('todos')) || []
+  theme.value = localStorage.getItem('theme') || 'normal'
 })
 
 //改变颜色方法
 const changeColor = async (event) => {
-  await nextTick(); // 等待下次 DOM 更新
   if (event.target.id === 'color1') {
       theme.value = 'normal'
     }
@@ -57,6 +74,7 @@ const changeColor = async (event) => {
 <template>
   <main class="app">
     <div :class="theme">
+      <!-- 颜色改变导航条 -->
     <div class="nav">
       <ul>
         <li @click="changeColor($event)" id="color1">Normal</li>
@@ -65,11 +83,18 @@ const changeColor = async (event) => {
       </ul>
     </div>
     
-      <section class="greeting">
+    <!-- 开场问候部分 -->
+    <section class="greeting">
       <h2 class="title">
-        GET READY?<input type="text" id="name" placeholder="name here" v-model="name">
+        Wassup?
+        <input 
+          type="text" 
+          id="name" 
+          placeholder="name here" 
+          v-model="name">
       </h2>
     </section>
+    <!-- 创建todo部分 -->
     <section class="create-todo">
       <h3>CREATE A TODO</h3>
       <form id="new-todo-form" @submit.prevent="addTodo">
@@ -91,6 +116,7 @@ const changeColor = async (event) => {
         <input type="submit" value="Add todo" />
       </form>
     </section>
+    <!-- todo列表展示 -->
     <section class="todo-list">
       <h3>TODO LIST BELOW</h3>
       <div class="list" id="todo-list">
@@ -112,6 +138,5 @@ const changeColor = async (event) => {
       </div>
     </section>
     </div>
-    
   </main>
 </template>
